fda0695e6ee8fe7ece2466143a99c44cd74822be
Andrew.Kieckhefer <finetco.eth@yahoo.com>
2026-06-17 10:41:38 -0600

    stable global template system + shared navigation update

--- thepolka.cloud/app.py
+++ thepolka.cloud/app.py
@@ -1,15 +1,15 @@
 #Force Flask to perform with the secure headers Cloudflare passes through
-from flask import Flask, render_template
+from flask import Flask,
 from werkzeug.middleware.proxy_fix import ProxyFix # <-- Add this import
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../shared/templates", static_folder="../static")
 
 # Tells Flask it is running behind an internet tunnel/proxy. Cloudflare tunnel safety headers
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 # Routes
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("home.html")
 
@@ -29,6 +29,20 @@ def contact():
 def résumé_generator():
     return render_template("résumé.html")
 
+from flask import request
+import os
+
+@app.route('/upload_logo_dev', methods=['POST'])
+def upload_logo_dev():
+    if 'file' not in request.files:
+        return "No file part", 400
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file", 400
+
+    target_path = os.path.expanduser('~/thepolka.cloud/thepolka.cloud/static/images/thepolka.cloud.png')
+    file.save(target_path)
+    return "File uploaded successfully!", 200
 # RUN SERVER
 
 if __name__ == "__main__":
